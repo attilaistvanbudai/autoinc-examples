15c258d3cad092810c676bfa4c52127b58b381d9
Zsolt_Kaufmann <Zsolt_Kaufmann@epam.com>
2014-12-15 16:31:39 +0100

    redundant locks removed

--- zsoltkaufmann.java
+++ zsoltkaufmann.java
@@ -18,13 +18,13 @@
     public String reverseWords(String s) {
 	if (s == null || s.length() == 0) {
 	    return "";
-	} // LOCKED
+	}
 	String[] arr = s.split(" ");
 	StringBuilder sb = new StringBuilder(); // LOCKED
 	for (int i = arr.length - 1; i >= 0; --i) {
 	    if (!arr[i].equals("")) {
 		sb.append(arr[i]).append(" ");
 	    }
-	} // LOCKED
+	}
 	return sb.length() == 0 ? "" : sb.substring(0, sb.length() - 1);
-    } // LOCKED
+    }
